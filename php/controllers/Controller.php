f21ffd9f80e225b7ec4c862ea440434aca75aa19
CyrilleDeletre <cyrille.deletre@.com>
2024-02-27 15:59:36 +0100

    corrections

--- php/controllers/Controller.php
+++ php/controllers/Controller.php
@@ -1,35 +1,39 @@
-<?php 
+<?php
 
 require_once "models/BddConnect.php";
 
-class Controller {
+class Controller
+{
 
-    function index(){
+    function index()
+    {
         require_once "views/base.php";
     }
 
-    function getTasks(){
+    function getTasks()
+    {
         $conn = new BddConnect;
         return $conn->readTask();
     }
 
-    function addTasks() {
-            $conn = new BddConnect();
-            $conn->insertTask($_POST['task']);
-            echo "Tâche ajoutée avec succès";
-        }
+    function addTasks()
+    {
+        $conn = new BddConnect();
+        $conn->insertTask($_POST['task']);
+        echo "Tâche ajoutée avec succès";
+    }
 
-    function deleteTasks(){
-            $conn = new BddConnect;
-            $conn->deleteTask($_POST['task_id']);
-            echo "Tâche supprimée avec succès";
-        }
+    function deleteTasks()
+    {
+        $conn = new BddConnect;
+        $conn->deleteTask($_POST['task_id']);
+        echo "Tâche supprimée avec succès";
+    }
 
-    function updateTasks(){
-            $conn = new BddConnect;
-            $conn->updateTask($_POST['task_id'], $_POST['new_task_name']);
-            echo "Tâche mise à jour avec succès";
-        }
+    function updateTasks()
+    {
+        $conn = new BddConnect;
+        $conn->updateTask($_POST['task_id'], $_POST['new_task_name']);
+        echo "Tâche mise à jour avec succès";
+    }
 }
-
-?>
